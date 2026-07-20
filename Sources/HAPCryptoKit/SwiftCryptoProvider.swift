36b727ca582f4cb34c54881f047d3639710ed891
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-20 00:30:24 -0400

    Adopt Foundation fallback in `SwiftCryptoProvider`

--- Sources/HAPCryptoKit/SwiftCryptoProvider.swift
+++ Sources/HAPCryptoKit/SwiftCryptoProvider.swift
@@ -1,7 +1,6 @@
 import BigInt
 import Crypto
 import Foundation
-import FoundationEmbedded
 import HAP
 
 /// The reference ``CryptoProvider`` for hosted platforms, backed by swift-crypto.
@@ -13,35 +12,35 @@ public struct SwiftCryptoProvider: CryptoProvider {
 
     // MARK: SHA-512
 
-    public func sha512(_ message: FoundationEmbedded.Data) -> FoundationEmbedded.Data {
-        FoundationEmbedded.Data(SHA512.hash(data: Foundation.Data(Array(message))))
+    public func sha512(_ message: Data) -> Data {
+        Data(SHA512.hash(data: Foundation.Data(Array(message))))
     }
 
     // MARK: HKDF-SHA-512
 
     public func hkdfSHA512(
-        inputKeyMaterial: FoundationEmbedded.Data,
-        salt: FoundationEmbedded.Data,
-        info: FoundationEmbedded.Data,
+        inputKeyMaterial: Data,
+        salt: Data,
+        info: Data,
         outputByteCount: Int
-    ) -> FoundationEmbedded.Data {
+    ) -> Data {
         let key = HKDF<SHA512>.deriveKey(
             inputKeyMaterial: SymmetricKey(data: Foundation.Data(Array(inputKeyMaterial))),
             salt: Foundation.Data(Array(salt)),
             info: Foundation.Data(Array(info)),
             outputByteCount: outputByteCount
         )
-        return key.withUnsafeBytes { FoundationEmbedded.Data(Array($0)) }
+        return key.withUnsafeBytes { Data(Array($0)) }
     }
 
     // MARK: ChaCha20-Poly1305
 
     public func seal(
-        _ message: FoundationEmbedded.Data,
-        key: FoundationEmbedded.Data,
-        nonce: FoundationEmbedded.Data,
-        authenticatedData: FoundationEmbedded.Data
-    ) throws(HAPError) -> FoundationEmbedded.Data {
+        _ message: Data,
+        key: Data,
+        nonce: Data,
+        authenticatedData: Data
+    ) throws(HAPError) -> Data {
         guard let nonceBytes = Self.normalizedNonce(Array(nonce)) else {
             throw .invalidData
         }
@@ -52,18 +51,18 @@ public struct SwiftCryptoProvider: CryptoProvider {
                 nonce: ChaChaPoly.Nonce(data: Foundation.Data(nonceBytes)),
                 authenticating: Foundation.Data(Array(authenticatedData))
             )
-            return FoundationEmbedded.Data(Array(sealedBox.ciphertext) + Array(sealedBox.tag))
+            return Data(Array(sealedBox.ciphertext) + Array(sealedBox.tag))
         } catch {
             throw .invalidData
         }
     }
 
     public func open(
-        _ ciphertext: FoundationEmbedded.Data,
-        key: FoundationEmbedded.Data,
-        nonce: FoundationEmbedded.Data,
-        authenticatedData: FoundationEmbedded.Data
-    ) throws(HAPError) -> FoundationEmbedded.Data {
+        _ ciphertext: Data,
+        key: Data,
+        nonce: Data,
+        authenticatedData: Data
+    ) throws(HAPError) -> Data {
         guard let nonceBytes = Self.normalizedNonce(Array(nonce)),
               ciphertext.count >= 16
         else { throw .invalidData }
@@ -79,7 +78,7 @@ public struct SwiftCryptoProvider: CryptoProvider {
                 using: SymmetricKey(data: Foundation.Data(Array(key))),
                 authenticating: Foundation.Data(Array(authenticatedData))
             )
-            return FoundationEmbedded.Data(message)
+            return Data(message)
         } catch {
             throw .notAuthorized
         }
@@ -97,35 +96,35 @@ public struct SwiftCryptoProvider: CryptoProvider {
 
     // MARK: Ed25519
 
-    public func makeEd25519PrivateKey() -> FoundationEmbedded.Data {
-        FoundationEmbedded.Data(Curve25519.Signing.PrivateKey().rawRepresentation)
+    public func makeEd25519PrivateKey() -> Data {
+        Data(Curve25519.Signing.PrivateKey().rawRepresentation)
     }
 
-    public func ed25519PublicKey(for privateKey: FoundationEmbedded.Data) -> FoundationEmbedded.Data {
+    public func ed25519PublicKey(for privateKey: Data) -> Data {
         let key = try! Curve25519.Signing.PrivateKey(
             rawRepresentation: Foundation.Data(Array(privateKey))
         )
-        return FoundationEmbedded.Data(key.publicKey.rawRepresentation)
+        return Data(key.publicKey.rawRepresentation)
     }
 
     public func ed25519Signature(
-        for message: FoundationEmbedded.Data,
-        privateKey: FoundationEmbedded.Data
-    ) throws(HAPError) -> FoundationEmbedded.Data {
+        for message: Data,
+        privateKey: Data
+    ) throws(HAPError) -> Data {
         do {
             let key = try Curve25519.Signing.PrivateKey(
                 rawRepresentation: Foundation.Data(Array(privateKey))
             )
-            return FoundationEmbedded.Data(try key.signature(for: Foundation.Data(Array(message))))
+            return Data(try key.signature(for: Foundation.Data(Array(message))))
         } catch {
             throw .invalidData
         }
     }
 
     public func ed25519IsValidSignature(
-        _ signature: FoundationEmbedded.Data,
-        for message: FoundationEmbedded.Data,
-        publicKey: FoundationEmbedded.Data
+        _ signature: Data,
+        for message: Data,
+        publicKey: Data
     ) -> Bool {
         guard let key = try? Curve25519.Signing.PublicKey(
             rawRepresentation: Foundation.Data(Array(publicKey))
@@ -138,21 +137,21 @@ public struct SwiftCryptoProvider: CryptoProvider {
 
     // MARK: X25519
 
-    public func makeCurve25519PrivateKey() -> FoundationEmbedded.Data {
-        FoundationEmbedded.Data(Curve25519.KeyAgreement.PrivateKey().rawRepresentation)
+    public func makeCurve25519PrivateKey() -> Data {
+        Data(Curve25519.KeyAgreement.PrivateKey().rawRepresentation)
     }
 
-    public func curve25519PublicKey(for privateKey: FoundationEmbedded.Data) -> FoundationEmbedded.Data {
+    public func curve25519PublicKey(for privateKey: Data) -> Data {
         let key = try! Curve25519.KeyAgreement.PrivateKey(
             rawRepresentation: Foundation.Data(Array(privateKey))
         )
-        return FoundationEmbedded.Data(key.publicKey.rawRepresentation)
+        return Data(key.publicKey.rawRepresentation)
     }
 
     public func curve25519SharedSecret(
-        privateKey: FoundationEmbedded.Data,
-        peerPublicKey: FoundationEmbedded.Data
-    ) throws(HAPError) -> FoundationEmbedded.Data {
+        privateKey: Data,
+        peerPublicKey: Data
+    ) throws(HAPError) -> Data {
         do {
             let key = try Curve25519.KeyAgreement.PrivateKey(
                 rawRepresentation: Foundation.Data(Array(privateKey))
@@ -161,7 +160,7 @@ public struct SwiftCryptoProvider: CryptoProvider {
                 rawRepresentation: Foundation.Data(Array(peerPublicKey))
             )
             let secret = try key.sharedSecretFromKeyAgreement(with: peer)
-            return secret.withUnsafeBytes { FoundationEmbedded.Data(Array($0)) }
+            return secret.withUnsafeBytes { Data(Array($0)) }
         } catch {
             throw .invalidData
         }
@@ -172,9 +171,9 @@ public struct SwiftCryptoProvider: CryptoProvider {
     public func srpVerifier(
         username: String,
         password: String,
-        salt: FoundationEmbedded.Data
-    ) -> FoundationEmbedded.Data {
-        FoundationEmbedded.Data(SRP6a.verifier(
+        salt: Data
+    ) -> Data {
+        Data(SRP6a.verifier(
             username: Array(username.utf8),
             password: Array(password.utf8),
             salt: Array(salt)
@@ -183,8 +182,8 @@ public struct SwiftCryptoProvider: CryptoProvider {
 
     public func makeSRPServer(
         username: String,
-        salt: FoundationEmbedded.Data,
-        verifier: FoundationEmbedded.Data
+        salt: Data,
+        verifier: Data
     ) throws(HAPError) -> SRP6aServer {
         var generator = SystemRandomNumberGenerator()
         let privateKey = (0 ..< 32).map { _ in UInt8.random(in: .min ... .max, using: &generator) }
@@ -192,7 +191,7 @@ public struct SwiftCryptoProvider: CryptoProvider {
             username: username,
             salt: salt,
             verifier: verifier,
-            privateKey: FoundationEmbedded.Data(privateKey)
+            privateKey: Data(privateKey)
         )
     }
 }
